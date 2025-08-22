923860b6575c4082c88e0bc2697d9b452f332f72
micasolimine <158102772+micasolimine@users.noreply.github.com>
2025-08-22 15:56:23 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-  e.preventDefault();
-  document.getElementById('mensajeConfirmacion').textContent = '¡Gracias por tu mensaje! Te responderé pronto.';
-  this.reset();
-});
